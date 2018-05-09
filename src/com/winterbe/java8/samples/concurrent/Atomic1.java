6286e65c36ca7e83705f750f6296c7d5f9aacd7c
nchang99 <nchang999@gmail.com>
2018-05-08 23:58:41 -0700

    Initial Commit

--- src/com/winterbe/java8/samples/concurrent/Atomic1.java
+++ src/com/winterbe/java8/samples/concurrent/Atomic1.java
@@ -1,5 +1,7 @@
 package com.winterbe.java8.samples.concurrent;
 
+//Git test
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
